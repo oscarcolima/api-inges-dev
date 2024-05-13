1500d536a1e8b969aee0c55ced8aae5b1f3d6464
TDTxLE <TDTxLE@gmail.com>
2024-05-13 00:10:48 -0600

    agrear panel

---
+++ Controllers/PanelController.cs
@@ -0,0 +1,28 @@
+using api_inges_dev.Context;
+using api_inges_dev.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace api_inges_dev.Controllers;
+
+[Route("api/[controller]")]
+// [Produces("application/json")]
+[ApiController]
+public class PanelController : ControllerBase
+{
+
+    private readonly ConexionSQLServer contex;
+
+    public PanelController(ConexionSQLServer contex)
+    {
+        this.contex = contex;
+    }
+
+    [HttpGet("getPanel")]
+    public ObjectResult getpanel()
+    {
+        var res = contex.Registers.ToList();
+        return Ok(res);
+    }
+}
