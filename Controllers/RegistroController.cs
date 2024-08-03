7dc79867d5e3c6f4f50c219b0947a34d74a2e1b9
tdtxle <tdtxle@gmail.com>
2024-08-03 10:27:10 -0600

    reportar datos

--- Controllers/RegistroController.cs
+++ Controllers/RegistroController.cs
@@ -4,6 +4,12 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 namespace api_inges_dev.Controllers;
 
 [Route("api/[controller]")]
@@ -26,13 +32,30 @@ public class RegistroController : ControllerBase
         return Ok(res);
     }
     [HttpPost("registrar")]
-    public Registro? registrar(Registro registro)
+    public async Task<Registro?> registrar(Registro registro)
     {
         contex.Registers.Add(registro);
         contex.SaveChanges();
 
+        var user = contex.Registers.OrderBy(r => r.id).Last();
 
-        return contex.Registers.OrderBy(r => r.id).Last();
+        var url = "https://hooks.zapier.com/hooks/catch/8944102/2ucbrwg/";
+
+
+        var json = JsonConvert.SerializeObject(user);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+
+        using (var client = new HttpClient())
+        {
+            var response = await client.PostAsync(url, content);
+            string responseString = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine($"Status Code: {response.StatusCode}");
+            Console.WriteLine($"Response: {responseString}");
+        }
+
+        return user;
     }
 
     [HttpGet("Getlevel")]
--- Models/login.cs
+++ Models/login.cs
@@ -3,7 +3,7 @@ namespace api_inges_dev.Models
 {
     public class Login
     {
-        string usuraio { get; set; }
-        string password { get; set; }
+        string? usuraio { get; set; }
+        string? password { get; set; }
     }
 }
